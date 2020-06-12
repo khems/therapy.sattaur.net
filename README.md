2c93aecbedc82edcd76aeaf423788a3e6b34a99f
Khem <hello@khems.net>
2020-06-12 11:43:19 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Website for advertising the services of Omar Sattaur
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <React.StrictMode>Omar Sattaur</React.StrictMode>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
